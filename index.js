16ec30c5a797e9ff0a9ea37a41fd3b61d3a7540a
NightLampGames <nightlampgames@gmail.com>
2023-10-06 00:11:56 +0200

    puska done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function hideEverything(){
     document.getElementById("npcList").style.display = "none";
     document.getElementById("cards").style.display = "none";
     document.getElementById("puska").style.display = "none";
+    document.getElementById("perception").src = src="https://www.youtube.com/embed/ZjlYFWLUDBQ?si=w5U1Lg7lNcQUJIh7&amp;controls=0&amp;autoplay=0";
     document.getElementById("perception").style.display = "none";
 }
 function showMain(){
@@ -16,13 +17,14 @@ function showMain(){
 }
 function showMemes(){
     hideEverything();
+    console.log(document.getElementById("perception").Status);
     document.getElementById("perception").style.display = "block"
     document.getElementById("perception").src = src="https://www.youtube.com/embed/ZjlYFWLUDBQ?si=w5U1Lg7lNcQUJIh7&amp;controls=0&amp;autoplay=1"
     document.querySelector(".navbar-collapse").classList.remove("show");
-    setTimeout(function (){
+    /*setTimeout(function (){
         document.getElementById("perception").src = src="https://www.youtube.com/embed/ZjlYFWLUDBQ?si=w5U1Lg7lNcQUJIh7&amp;controls=0&amp;autoplay=0";
         showMain();
-    },111000);
+    },111000);*/
 }
 function showCards(){
     hideEverything();
@@ -50,53 +52,26 @@ function showNpc(){
     document.querySelector(".navbar-collapse").classList.remove("show");
    
 }
-addEventListener("click",function(event){
-    console.log()
-})
-addEventListener("keydown",function(event){
-    makeSound(event.key);
-    buttonAnimation(event.key);
-})
-function makeSound(key){
-    switch (key) {
-        case "w":
-            var tom1 = new Audio("./sounds/tom-1.mp3");
-            tom1.play();
-            break;
-        case "a":
-            var tom2 = new Audio("./sounds/tom-2.mp3");
-            tom2.play();
-            break;
-        case "s":
-            var tom3 = new Audio("./sounds/tom-3.mp3");
-            tom3.play();
-            break;
-        case "d":
-            var tom4 = new Audio("./sounds/tom-4.mp3");
-            tom4.play();
-            break;
-        case "j":
-            var crash = new Audio("./sounds/crash.mp3");
-            crash.play();
-            break;
-        case "k":
-            var kick = new Audio("./sounds/kick-bass.mp3");
-            kick.play();
-            break;
-        case "l":
-            var snare = new Audio("./sounds/snare.mp3");
-            snare.play();
-            break;
-           
-        default:
-            console.log(this);
-            break;
+function showTab(tab){
+    for(let i=0; i<3;i++){
+        document.getElementsByClassName("nav-link")[i].classList.remove("active");
+        document.getElementsByClassName("tabs")[i].style.display="none";
     }
-}
-function buttonAnimation(key){
-    var activeBtn = document.querySelector("."+key);
-    activeBtn.classList.add("pressed");
-    setTimeout(() => { activeBtn.classList.remove("pressed"); }, 100);
+    
+    //document.getElementsByClassName("tabs").style.display="none";
+    document.getElementById("current").innerHTML=document.getElementById(tab).innerHTML;
+    switch (tab) {
+        case "action":
+            document.getElementById("a").classList.add("active");
+            break;
+            case "else":
+                document.getElementById("e").classList.add("active");
+                break;
+            
+        default:
+            document.getElementById("m").classList.add("active");
+    }
+
 }
 
 
